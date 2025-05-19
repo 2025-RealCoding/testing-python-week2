8534a239550bd1e27bfa7633159caf1dbc279345
haaaanung <hanung510@naver.com>
2025-05-19 17:17:57 +0900

    change function name

--- tests/test_service_users.py
+++ tests/test_service_users.py
@@ -52,7 +52,7 @@ def test_delete_registered_user(setup_user_db):
     user_info = get_user(user_id)
     assert user_info is None
 
-def test_authenticate_deleted_user(setup_user_db):
+def test_non_registered_user(setup_user_db):
     user_id = "haaaanung"
 
     user_info = get_user(user_id)
