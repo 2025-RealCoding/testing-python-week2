827dc659a8dd43d7503534ed23e37b3654c833d6
KwonHyeokju <162686659+mungtaengi124@users.noreply.github.com>
2025-05-22 00:50:45 +0900

    Update face_embedding.py

--- app/face/face_embedding.py
+++ app/face/face_embedding.py
@@ -1,8 +1,12 @@
-def extract_embedding(image):
-    """이미지에서 임베딩 추출"""
-    pass
+import numpy as np
+from deepface import DeepFace
+from typing import Union
+
+def extract_embedding(img_path: Union[str, np.ndarray]) -> list:
+    embedding = DeepFace.represent(img_path)[0]['embedding']
+    return embedding
 
 
-def verify_embedding(embedding1, embedding2):
-    """두 임베딩이 같은 사람인지 검증"""
-    pass
+def verify_embedding(embedding1: list, embedding2: list) -> bool:
+    is_same_person = DeepFace.verify(embedding1, embedding2)['verified']
+    return is_same_person
